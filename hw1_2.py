754b9de9269a6954a0f87dd95e87a30c0612c14e
stone971275 <160737121+stone971275@users.noreply.github.com>
2024-04-08 22:46:11 +0800

    Add files via upload

--- hw1_2.py
+++ hw1_2.py
@@ -1,6 +1,15 @@
-def hw1_2():
-    output = 0
-    #-------------------------------
-    # output = # ....
-    #-------------------------------
+def hw1_2(a,b):
+    output = 2*a / 2**b
+    """
+    Parameters:
+    a (float): voltage_range
+    b (int): resolution_bits
+    Returns:
+    float: output calculated based on the formula
+   """
     return output
+a = 10.0
+b = 14.0
+
+result = hw1_2(a, b)
+print(result)
